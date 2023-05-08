4f3722fb61269c57e006665143c6cd54f37d66ca
Petr Alekseev <petr.alekseev@ntprog.com>
2023-05-08 15:20:21 +0300

    trying to add threading

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+from threading import Thread, active_count
 from chat_lib import Message, Transport
 
 HOST = '127.0.0.1'
@@ -9,29 +10,30 @@ CONNECTION_ERRORS = (
     ConnectionResetError
 )
 
+def handle_single_client(connection, address):
+    inc_addr, inc_port = address
+    print(f'Connection accepted from {inc_addr}:{inc_port}')
 
+    try:
+        inc_str = Transport.receive(connection)
+        inc_msg = Message().unpack(inc_str)
+        print(f'Message received:\n{inc_msg}')
+    except CONNECTION_ERRORS:
+        print(f'{inc_addr}:{inc_port} has disconnected.')
+
+    username, text = inc_msg['username'], inc_msg['text']
+    out_str = Message(username, text).pack()
+    Transport.send(connection, out_str)
+    
 if __name__ == '__main__':
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.bind((HOST, PORT))
         sock.listen()
         print(f'Server started at {HOST}:{PORT}')
-
-        connection, address = sock.accept()
-        inc_addr, inc_port = address
-
-        with connection:
-            print(f'Connection accepted from {inc_addr}:{inc_port}')
-
-            while True:
-                try:
-                    inc_str = Transport.receive(connection)
-                except CONNECTION_ERRORS:
-                    print(f'{inc_addr}:{inc_port} has disconnected.')
-                    break
-                inc_msg = Message().unpack(inc_str)
-                print(f'Message received:\n{inc_msg}')
-                
-                username, text = inc_msg['username'], inc_msg['text']
-                out_str = Message(username, text).pack()
-                Transport.send(connection, out_str)
+        connections_list = []
+        while True:
+            connection, address = sock.accept()
+            connections_list.append(connection)
+            thread = Thread(target=handle_single_client, args=(connection, address))
+            thread.start()
+            print(f'Active connections: {active_count() - 1}')
