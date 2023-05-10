a6a456daa90d4a0e9a6ec6aa224b5fccfae8f14f
Petr Alekseev <petr.alekseev@ntprog.com>
2023-05-10 20:20:55 +0300

    server is a class now

--- server.py
+++ server.py
@@ -2,51 +2,59 @@ import socket
 from threading import Thread, active_count
 from chat_lib import Message, Transport
 
-HOST = '127.0.0.1'
-PORT = 55555
-
 CONNECTION_ERRORS = (
     ConnectionAbortedError,
     ConnectionResetError
 )
-CONNECTIONS_LIST = []
 
-def handle_single_client(connection: socket, address: tuple) -> None:
-    inc_addr, inc_port = address
-    print(f'[CON_ACPT] Connection accepted from {inc_addr}:{inc_port}')
+HOST = '127.0.0.1'
+PORT = 55555
 
-    while connection:
-        try:
-            inc_str = Transport.receive(connection)
-            inc_msg = Message().unpack(inc_str)
-            print(f'[MSG_RECV] Message received:\n{inc_msg}')
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.connections = []
 
-            username, text = inc_msg['username'], inc_msg['text']
-            out_str = Message(username, text).pack()
+    def handle_single_client(self, connection, address):
+        inc_addr, inc_port = address
+        print(f'[CON_ACPT] Connection accepted from {inc_addr}:{inc_port}')
 
-            print(f'[MSG_SEND] Message ({out_str}) is being sent to:')
-            for con in CONNECTIONS_LIST:
-                Transport.send(con, out_str)
-                print(con)
+        while connection:
+            try:
+                inc_str = Transport.receive(connection)
+                inc_msg = Message().unpack(inc_str)
+                print(f'[MSG_RECV] Message received:\n{inc_msg}')
 
-            print('[MSG_SEND] Sending end.')
+                username, text = inc_msg['username'], inc_msg['text']
+                out_str = Message(username, text).pack()
 
-        except CONNECTION_ERRORS:
-            print(f'[CON_DISC] {inc_addr}:{inc_port} has disconnected.')
-            CONNECTIONS_LIST.remove(connection)
-            print(f'[CON_STAT] Active connections: {active_count() - 1}')
-            break
+                print(f'[MSG_SEND] Message ({out_str}) is being sent to:')
+                for con in self.connections:
+                    Transport.send(con, out_str)
+                    print(con)
 
+                print('[MSG_SEND] Sending end.')
+
+            except CONNECTION_ERRORS:
+                print(f'[CON_DISC] {inc_addr}:{inc_port} has disconnected.')
+                self.connections.remove(connection)
+                print(f'[CON_STAT] Active connections: {active_count() - 1}')
+                break
+
+    def start(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.bind((self.host, self.port))
+            sock.listen()
+            print(f'[SRV_STAT] Server started at {self.host}:{self.port}')
+
+            while True:
+                connection, address = sock.accept()
+                self.connections.append(connection)
+                thread = Thread(target=self.handle_single_client, args=(connection, address))
+                thread.start()
+                print(f'[CON_STAT] Active connections: {active_count() - 1}')
 
 if __name__ == '__main__':
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.bind((HOST, PORT))
-        sock.listen()
-        print(f'[SRV_STAT] Server started at {HOST}:{PORT}')
-
-        while True:
-            connection, address = sock.accept()
-            CONNECTIONS_LIST.append(connection)
-            thread = Thread(target=handle_single_client, args=(connection, address))
-            thread.start()
-            print(f'[CON_STAT] Active connections: {active_count() - 1}')
+    server = Server(HOST, PORT)
+    server.start()
